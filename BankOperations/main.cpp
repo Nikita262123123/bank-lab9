722294d30a0427c1f70dac5f9430d717ea4eba13
Nikita <121441784+Nikita262123123@users.noreply.github.com>
2022-12-26 18:26:51 +0300

    Добавлен вывод общей информации о задании

--- BankOperations/main.cpp
+++ BankOperations/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #9. GIT\n";
+    cout << "Variant #7. BankOperations\n";
+    cout << "Author: Nikita Labuts\n";
+    
     return 0;
 }
